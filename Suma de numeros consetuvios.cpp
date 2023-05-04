b73c024f5589ee3a8fb0225d424853f86446e416
canchingrer <ronny.canchingre.manzaba@utelvt.edu.ec>
2023-05-04 14:49:39 -0500

    Mi primer proyecto

---
+++ Suma de numeros consetuvios.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main ()
+{
+    int n, suma=0;
+    cout <<"Ingrese el numero";
+    cin >>n;
+    for (int i=1; i<=n; i++)
+    suma += i;
+    {
+        cout <<"La suma es:"<<suma<<endl;
+        getch();
+        return 0;
+    }
+}
